ff0daced3e80c88d3a3cfe90a438ec472be7a961
nayak <wjdduq914@gmail.com>
2017-11-04 17:34:45 +0900

    📚Update. winston logger config (check process.env.NODE_ENV)

--- config/logger.js
+++ config/logger.js
@@ -3,21 +3,27 @@
 const winston = require('winston');
 const moment = require('moment');
 
+/**
+ * If you use winston logger,
+ * you must make the 'logFiles' directory at your project root.
+ */
 const logger = new winston.Logger({
-  transports : [
-    new (winston.transports.Console)({
-      level: 'info',
-      colorize: true
-    })
-    /* only used server */
-    //new (require('winston-daily-rotate-file'))({
-    //    level: 'info',
-    //    filename: './logFiles/logging-',
-    //    maxsize: 1000 * 1024,
-    //    datePattern: 'yyyy-MM-dd.log',
-    //    json: false,
-    //    timestamp: () => {return moment().format('YYYY-MM-DD HH:mm:ss');}
-    //})
+  transports: [
+    process.env.NODE_ENV === 'production' ?
+      /* only used server */
+      new (require('winston-daily-rotate-file'))({
+        level: 'info',
+        filename: './logFiles/logging-',
+        maxsize: 1000 * 1024,
+        datePattern: 'yyyy-MM-dd.log',
+        json: false,
+        timestamp: () => {return moment().format('YYYY-MM-DD HH:mm:ss');}
+      })
+      :
+      new (winston.transports.Console)({
+        level: 'info',
+        colorize: true
+      })
   ]
 });
 
